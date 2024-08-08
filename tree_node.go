0b51e24eefba52baae54c6c476addf86c658a32f
ihewe <ihewe001@gmail.com>
2024-08-08 17:51:45 +0800

    tree node

---
+++ tree_node.go
@@ -0,0 +1,22 @@
+package excel_import
+
+type treeNode struct {
+	key      string
+	parent   *treeNode
+	rank     int
+	children []*treeNode
+	item     any
+}
+
+func constructLevelNode(s []string, parent *treeNode, level int) {
+	m := make(map[string]bool)
+	for _, v := range s {
+		if _, ok := m[v]; ok {
+			continue
+		}
+		m[v] = true
+		parent.children = append(parent.children, &treeNode{parent: parent, rank: level, key: v})
+	}
+
+	return
+}
