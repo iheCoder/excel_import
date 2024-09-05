c2ab3d80dba27e8e6834382ba96e88f77a9e878c
heliwei <heliwei@tal.com>
2024-09-05 16:29:55 +0800

    support gorm tag parse

--- model.go
+++ model.go
@@ -29,3 +29,33 @@ type ExcelImportTagAttr struct {
 func CheckChkKeyMatch(cm CheckMode, key string) bool {
 	return cm == CheckMode(key)
 }
+
+type GormTag struct {
+	// The column name of the database.
+	// tagName: column
+	Column string
+
+	// The primary key of the database.
+	// tagName: primary_key
+	PrimaryKey bool
+
+	// The auto increment of the database.
+	// tagName: auto_increment
+	AutoIncrement bool
+
+	// The default value of the database.
+	// tagName: default
+	Default string
+
+	// The not null of the database.
+	// tagName: not null
+	NotNull bool
+
+	// The size of the database.
+	// tagName: size
+	Size int
+
+	// The type of the database.
+	// tagName: type
+	Type string
+}
--- utils/tag_parser.go
+++ utils/tag_parser.go
@@ -10,6 +10,7 @@ import (
 const (
 	excelImportTag = "exi"
 	invalidIndex   = -1
+	gormTag        = "gorm"
 )
 
 func ParseTag(st any) []*excel_import.ExcelImportTagAttr {
@@ -93,6 +94,85 @@ func parseTag(tag string) *excel_import.ExcelImportTagAttr {
 			tagAttr.Check = excel_import.CheckMode(value)
 		case "tree":
 			tagAttr.Tree = excel_import.TreeFlag(value)
+		}
+	}
+
+	return tagAttr
+}
+
+func ParseGormTag(st any) []*excel_import.GormTag {
+	// get struct if st is a pointer
+	if reflect.TypeOf(st).Kind() == reflect.Ptr {
+		st = reflect.ValueOf(st).Elem().Interface()
+	}
+
+	// get reflect type
+	t := reflect.TypeOf(st)
+
+	// get the number of fields
+	numField := t.NumField()
+
+	// create a slice to store the tag attributes
+	tagAttrs := make([]*excel_import.GormTag, 0, numField)
+
+	// iterate over the fields
+	for i := 0; i < numField; i++ {
+		// get the field
+		field := t.Field(i)
+
+		// get the tag
+		tag := field.Tag.Get(gormTag)
+
+		// parse the tag
+		tagAttr := parseGormTag(tag)
+
+		// append the tag attributes to the slice
+		tagAttrs = append(tagAttrs, tagAttr)
+	}
+
+	return tagAttrs
+}
+
+func parseGormTag(tag string) *excel_import.GormTag {
+	// create a new tag attribute
+	tagAttr := &excel_import.GormTag{}
+
+	// handle the case when the tag is empty
+	if len(tag) == 0 {
+		return tagAttr
+	}
+
+	// split the tag by comma
+	tagParts := strings.Split(tag, ",")
+
+	// iterate over the tag parts
+	for _, part := range tagParts {
+		// split the part by colon
+		partParts := strings.Split(part, ":")
+
+		// get the key and value
+		key := partParts[0]
+		value := partParts[1]
+
+		// set the key and value to the tag attribute
+		switch key {
+		case "column":
+			tagAttr.Column = value
+		case "primary_key":
+			tagAttr.PrimaryKey = true
+		case "auto_increment":
+			tagAttr.AutoIncrement = true
+		case "default":
+			tagAttr.Default = value
+		case "not null":
+			tagAttr.NotNull = true
+		case "size":
+			size, err := strconv.Atoi(value)
+			if err == nil {
+				tagAttr.Size = size
+			}
+		case "type":
+			tagAttr.Type = value
 		}
 	}
 
