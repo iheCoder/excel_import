c7c211498a60bcdd18afd6a14562d675a9b6816b
ihewe <ihewe001@gmail.com>
2024-09-06 23:45:12 +0800

    err builder

--- utils/err_handle.go
+++ utils/err_handle.go
@@ -9,6 +9,49 @@ const (
 	rowErrSep = ", "
 )
 
+type ErrBuilder struct {
+	errs    []error
+	content string
+}
+
+func NewErrBuilder() *ErrBuilder {
+	return &ErrBuilder{
+		errs: make([]error, 0),
+	}
+}
+
+func (eb *ErrBuilder) Add(err error) {
+	if err == nil {
+		return
+	}
+
+	eb.errs = append(eb.errs, err)
+}
+
+func (eb *ErrBuilder) AddContent(content string) {
+	eb.content = content
+}
+
+func (eb *ErrBuilder) Build() error {
+	if len(eb.errs) == 0 {
+		return nil
+	}
+
+	errStr := eb.content
+	if errStr == "" {
+		errStr = "数据错误: "
+	}
+
+	for _, err := range eb.errs {
+		if err == nil {
+			continue
+		}
+		errStr += err.Error() + "; "
+	}
+
+	return errors.New(errStr)
+}
+
 func CombineErrors(row int, errs ...error) error {
 	errStr := fmt.Sprintf("第%d行数据错误: ", row+1)
 	for _, err := range errs {
