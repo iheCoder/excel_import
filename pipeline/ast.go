8b3e15f0b0a1c1bc070a986eff808e21d08f37f2
ihewe <ihewe001@gmail.com>
2024-09-09 00:01:26 +0800

    CreateStructDecl

--- pipeline/ast.go
+++ pipeline/ast.go
@@ -13,9 +13,27 @@ type AstFile struct {
 	file *ast.File
 }
 
+type StructInfo struct {
+	Name   string
+	Fields []Field
+}
+
+type Field struct {
+	Name    string
+	Type    string
+	Comment string
+}
+
+// CreateImportDecl creates an import declaration with the given import paths.
 func CreateImportDecl(imports []string) ast.Decl {
 	specs := make([]ast.Spec, len(imports))
 	for i, imp := range imports {
+		// If the import path is empty, create an empty import spec.
+		if len(imp) == 0 {
+			specs[i] = &ast.ImportSpec{}
+			continue
+		}
+
 		specs[i] = &ast.ImportSpec{
 			Path: &ast.BasicLit{
 				Kind:  token.STRING,
@@ -27,5 +45,31 @@ func CreateImportDecl(imports []string) ast.Decl {
 	return &ast.GenDecl{
 		Tok:   token.IMPORT,
 		Specs: specs,
+	}
+}
+
+// CreateStructDecl creates a struct declaration with the given struct info.
+func CreateStructDecl(info *StructInfo) ast.Decl {
+	// Create a field list with the given fields.
+	fields := make([]*ast.Field, len(info.Fields))
+	for i, field := range info.Fields {
+		fields[i] = &ast.Field{
+			Names: []*ast.Ident{ast.NewIdent(field.Name)},
+			Type:  ast.NewIdent(field.Type),
+			Doc:   &ast.CommentGroup{List: []*ast.Comment{{Text: fmt.Sprintf("// %s", field.Comment)}}},
+		}
+	}
+
+	// Create a type spec with the given struct name and fields.
+	return &ast.GenDecl{
+		Tok: token.TYPE,
+		Specs: []ast.Spec{
+			&ast.TypeSpec{
+				Name: ast.NewIdent(info.Name),
+				Type: &ast.StructType{
+					Fields: &ast.FieldList{List: fields},
+				},
+			},
+		},
 	}
 }
