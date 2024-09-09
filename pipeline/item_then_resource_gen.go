a5fa1992b2f5f5c1fe73c63e95f2a32768805658
ihewe <ihewe001@gmail.com>
2024-09-10 00:04:44 +0800

    AddStructDecl

--- pipeline/item_then_resource_gen.go
+++ pipeline/item_then_resource_gen.go
@@ -9,12 +9,19 @@ var (
 		"",
 		"excel_import/general_framework",
 	}
+	structName = "SectionImporter"
 )
 
 type ItemResourceAstGenerator struct {
 	f *ast.File
 }
 
-func (i *ItemResourceAstGenerator) CreateImportDecl() {
+func (i *ItemResourceAstGenerator) AddImportDecl() {
 	i.f.Decls = append(i.f.Decls, CreateImportDecl(importsStmt))
+}
+
+func (i *ItemResourceAstGenerator) AddStructDecl() {
+	i.f.Decls = append(i.f.Decls, CreateStructDecl(&StructInfo{
+		Name: structName,
+	}))
 }
