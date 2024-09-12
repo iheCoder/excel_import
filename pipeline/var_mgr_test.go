78cf0d655a8199dd6cd5051bdac34ffd0bd814d1
iheCoder <ihewe001@163.com>
2024-09-12 18:08:31 +0800

    TestCheckVarConflictInScope

--- pipeline/var_mgr_test.go
+++ pipeline/var_mgr_test.go
@@ -121,3 +121,54 @@ func TestGenerateVarNameByLowerFirst(t *testing.T) {
 		}
 	}
 }
+
+func TestCheckVarConflictInScope(t *testing.T) {
+	v := NewVarMgr()
+	childA := "childA"
+	childB := "childB"
+	grandChildA := "grandChildA"
+	grandChildB := "grandChildB"
+	grandChildC := "grandChildC"
+
+	v.AddScopeAtRoot(childA)
+	v.AddScopeAtRoot(childB)
+	v.AddScope(grandChildA, childA)
+	v.AddScope(grandChildB, childA)
+	v.AddScope(grandChildC, childB)
+
+	v.AddVarInScope("root", "")
+	v.AddVarInScope("childA", "childA")
+	v.AddVarInScope("childB", "childB")
+	v.AddVarInScope("grandChildA", "grandChildA")
+	v.AddVarInScope("grandChildB", "grandChildB")
+	v.AddVarInScope("grandChildC", "grandChildC")
+
+	// should be no conflict
+	noConflictVarName := "noConflict"
+	if got := v.checkVarConflictInScope(noConflictVarName, v.findScope("")); got {
+		t.Fatalf("expect false, but got true")
+	}
+
+	// should be conflict with childA
+	rootVarName := "root"
+	if got := v.checkVarConflictInScope(rootVarName, v.findScope(childA)); !got {
+		t.Fatalf("expect true, but got false")
+	}
+
+	// should be no conflict with childB
+	childAVarName := "childA"
+	if got := v.checkVarConflictInScope(childAVarName, v.findScope(childB)); got {
+		t.Fatalf("expect false, but got true")
+	}
+
+	// should be conflict with childA
+	grandChildAVarName := "grandChildA"
+	if got := v.checkVarConflictInScope(grandChildAVarName, v.findScope(childA)); !got {
+		t.Fatalf("expect true, but got false")
+	}
+
+	// should be conflict with root
+	if got := v.checkVarConflictInScope(grandChildAVarName, v.findScope("")); !got {
+		t.Fatalf("expect true, but got false")
+	}
+}
